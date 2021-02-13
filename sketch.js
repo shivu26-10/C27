da411ce76db54accf0fffea3aecbf37ee69adbff
shivu26-10 <svchandgude2020@gmail.com>
2021-02-13 12:06:30 +0530

    created Constant

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +39,10 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    log6 = new Log(230,180,80,PI/2);
+
+    chain = new Chain(bird.body,log6.body);
+    
 }
 
 function draw(){
@@ -61,4 +68,8 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    log6.display();
+     
+    chain.display();
 }
